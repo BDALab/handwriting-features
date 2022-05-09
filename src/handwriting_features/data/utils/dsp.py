db97884ba3b29b3c0166e3b11952cdfe9aa2c553
ZvoV <vojtech.zvoncak@gmail.com>
2022-05-09 10:11:33 +0200

    Implementation of filtering functions in dsp.py

--- src/handwriting_features/data/utils/dsp.py
+++ src/handwriting_features/data/utils/dsp.py
@@ -1,3 +1,6 @@
+import scipy.signal as sc
+import numpy as np
+
 class LowPassFilter(object):
     """Class implementing the low-pass filter"""
 
@@ -5,10 +8,23 @@ class LowPassFilter(object):
         self.fs = fs
         self.fc = fc
 
-    # TODO: in progress
+    def _butter_lowpass_filter(self, data, cutoff, fs, order=10):
+        """Private function performing low-pass filtering using Butterworth filter"""
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = sc.butter(order, normal_cutoff, btype='lowpass', analog=False)
+        return sc.filtfilt(b, a, data)
+
+    def _bessel_lowpass_filter(self, data, cutoff, fs, order=10):
+        """Private function performing low-pass filtering using Bessel filter"""
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = sc.bessel(order, normal_cutoff, btype='lowpass', analog=False)
+        return sc.filtfilt(b, a, data)
+
     def filter(self, signal):
         """Filters an input signal by a low-pass filter"""
-        return signal
+        return self._butter_lowpass_filter(signal, self.fc, self.fs)
 
 
 class GaussianFilter(object):
@@ -18,7 +34,12 @@ class GaussianFilter(object):
         self.fs = fs
         self.n = n
 
-    # TODO: in progress
+    def _custom_gaussian_filter(self, data, n_window=50, sigma=10):
+        """Filters an input signal by Gaussian window"""
+        window = sc.windows.gaussian(n_window, std=sigma)
+        window = window / np.sum(window)
+        return sc.filtfilt(window, 1, data)
+
     def filter(self, signal):
         """Filters an input signal by a Gaussian filter"""
-        return signal
+        return self._custom_gaussian_filter(signal, self.n)
