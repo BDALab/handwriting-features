148f99561534f220aeebcf7c4c6ede3046353cc4
Zoltán Galáž <zoltan@galaz.eu>
2022-05-09 11:21:34 +0200

    Final updates of the filter classes

--- src/handwriting_features/data/utils/dsp.py
+++ src/handwriting_features/data/utils/dsp.py
@@ -1,6 +1,7 @@
 import scipy.signal as sc
 import numpy as np
 
+
 class LowPassFilter(object):
     """Class implementing the low-pass filter"""
 
@@ -8,19 +9,30 @@ class LowPassFilter(object):
         self.fs = fs
         self.fc = fc
 
-    def _butter_lowpass_filter(self, data, cutoff, fs, order=10):
-        """Private function performing low-pass filtering using Butterworth filter"""
-        nyq = 0.5 * fs
-        normal_cutoff = cutoff / nyq
-        b, a = sc.butter(order, normal_cutoff, btype='lowpass', analog=False)
-        return sc.filtfilt(b, a, data)
+    @staticmethod
+    def _get_wn(cutoff, fs):
+        """Private method returning the normalized cutoff frequency"""
+        return cutoff / (0.5 * fs)
 
-    def _bessel_lowpass_filter(self, data, cutoff, fs, order=10):
-        """Private function performing low-pass filtering using Bessel filter"""
-        nyq = 0.5 * fs
-        normal_cutoff = cutoff / nyq
-        b, a = sc.bessel(order, normal_cutoff, btype='lowpass', analog=False)
-        return sc.filtfilt(b, a, data)
+    @classmethod
+    def _butter_lowpass_filter(cls, data, cutoff, fs, order=10):
+        """Private method performing low-pass filtering using Butterworth filter"""
+
+        # Prepare the coefficients
+        coefficients = sc.butter(order, cls._get_wn(cutoff, fs), btype="lowpass", analog=False, output="ba")
+
+        # Filter the input signal
+        return sc.filtfilt(*coefficients, data)
+
+    @classmethod
+    def _bessel_lowpass_filter(cls, data, cutoff, fs, order=10):
+        """Private method performing low-pass filtering using Bessel filter"""
+
+        # Prepare the coefficients
+        coefficients = sc.bessel(order, cls._get_wn(cutoff, fs), btype="lowpass", analog=False, output="ba")
+
+        # Filter the input signal
+        return sc.filtfilt(*coefficients, data)
 
     def filter(self, signal):
         """Filters an input signal by a low-pass filter"""
@@ -34,10 +46,15 @@ class GaussianFilter(object):
         self.fs = fs
         self.n = n
 
-    def _custom_gaussian_filter(self, data, n_window=50, sigma=10):
-        """Filters an input signal by Gaussian window"""
+    @classmethod
+    def _custom_gaussian_filter(cls, data, n_window=50, sigma=10):
+        """Private function performing Gaussian filtration"""
+
+        # Prepare the Gaussian window
         window = sc.windows.gaussian(n_window, std=sigma)
         window = window / np.sum(window)
+
+        # Filter the input signal
         return sc.filtfilt(window, 1, data)
 
     def filter(self, signal):
